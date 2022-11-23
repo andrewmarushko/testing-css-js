9d1ceba64c43bd00813368a9dcf995db68a0a102
 Oleksandr Mykhailovskyi <oleksandr.mykhailovskyi@megadevllc.com>
2022-11-24 00:11:06 +0200

    develop working on hero section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+//css
 import "./styles/css/main.css";
-import "./styles/sass/style.sass";
+//sass
+// import "./styles/sass/components/common.sass";
+// import "./styles/sass/components/header.sass";
+// import "./styles/sass/components/hero.sass";
+import "./styles/sass/main.sass"
+//js
 import './js/common'
+import './js/header'
+import './js/hero'
--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,36 @@
-let add = (a,b) => a+b
-console.log(add(2,6))
+const navList = document.getElementById("nav-list");
+
+// the function to remove active class in navbar
+const removeClasses = (htmlCollection) => {
+  Array.from(htmlCollection).forEach((item) => {
+    item.classList.remove('active')
+  })
+}
+
+//this handler adds active styles for the nav item
+const navListHandler = (e) => {
+  removeClasses(navList.children)
+  if(e.target.tagName==='A') {
+    const targetListItem= e.target.parentElement
+    targetListItem.classList.add("active")
+  }
+  if(e.target.tagName==='LI') {
+    e.target.classList.add("active")
+  }
+};
+
+navList.addEventListener("click", navListHandler)
+
+//---------------
+
+// toggle burger menu
+const navItems = document.querySelectorAll(".nav-item");
+const burgerMenu = document.querySelector(".header__burger");
+
+const burgerMenuHandler = () => {
+  navItems.forEach((item) => {
+    item.classList.toggle("hidden");
+  });
+};
+
+burgerMenu.addEventListener("click", burgerMenuHandler);
